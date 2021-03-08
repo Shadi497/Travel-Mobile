0b0b59e0d4cb7e81aa82ea64fcc914f9f58c4119
Shadi497 <b.shadi.h@gmail.com>
2021-03-08 11:46:42 +0300

    Setup, Authentication & Screens

---
+++ components/Navigation/index.js
@@ -0,0 +1,35 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import Home from "../Home";
+import SignIn from "../Authentication/SignIn";
+import SignUp from "../Authentication/SignUp";
+
+export const RootNavigator = () => {
+  const { Navigator, Screen } = createStackNavigator();
+
+  return (
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerTitleAlign: "center" }}
+    >
+      <Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Screen
+        name="SignIn"
+        component={SignIn}
+        options={{
+          title: "Sign In",
+          headerShown: false,
+        }}
+      />
+
+      <Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+          title: "Sign Up",
+          headerShown: false,
+        }}
+      />
+    </Navigator>
+  );
+};
